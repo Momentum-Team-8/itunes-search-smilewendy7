58f63d2137e96c5fe53bf570fe7fc5a1a7ff0666
smilewendy7 <smilewendy7@gmail.com>
2021-06-02 23:10:58 -0400

    fix the issue of search  reset

--- main.js
+++ main.js
@@ -1,8 +1,9 @@
-// reset when searching something new???? 
-// search + ???
-//favicon where is it?
+//to do list: 
+    // search + ???
+    //favicon where is it?
+    //add lines to seperate sections ... 
 
-const url = "https://itunes.apple.com/search?"
+const url = "https://proxy-itunes-api.glitch.me/search?"
 const inputContent = document.querySelector('#search-field').value
 
 const form = document.querySelector('.search-form')
@@ -15,45 +16,51 @@ form.addEventListener('submit', event=> {
     searchSongs()
   })
 
+//search reset 
+form.addEventListener('reset', event=> {
+    location.reload()
+  })
 
 //get the list of searching results 
 function searchSongs(){
-    //set the JSON to empty first???????????
-    json = {}
-    // ??? define .value inside???? 
     const inputContent = document.querySelector('#search-field').value
     fetch (url + "entity=song&" + "term="+ `${inputContent}`+ "&limit=10")
     .then(res => res.json())
     .then(data=> {
         // console.log(data)
         console.log(data)
-        console.log("this is the url when searching", url + "entity=song&" + "term="+ `${inputContent}`+ "limit=10")
+        console.log("this is the url when searching", url + "entity=song&" + "term="+ `${inputContent}`+ "&limit=10")
 
+        let i = 0
         for (let result of data.results){
             // console.log(result)
-            console.log("here is data", result.artworkUrl100)
-            renderMusic(result)
+            // console.log("here is data", result.artworkUrl100)
+            renderMusic(result, i)
+            i = i + 1
         }
+        // console.log("END: " + data.results[0].artworkUrl30)
     })
 } 
 
 // display photo and link for each of the song 
 
-function renderMusic(music) {
+function renderMusic(music, index) {
     const eachMusic= document.createElement('li')
-    eachMusic.setAttribute("id", "each-music")
+    eachMusic.setAttribute("id", `${index}`)
+    eachMusic.setAttribute("class", "each-music")
     musicListing.appendChild(eachMusic)
+
     // *** name of artist *** 
     const musicArtist = document.createElement('p')
     musicArtist.setAttribute("id", "artist-name")
     eachMusic.appendChild(musicArtist)
     musicArtist.innerText= `${music.artistName}`
 
-    // *** name of song *** 
-const musicName = document.createElement('p')
-musicName.setAttribute("id", "music-name")
-eachMusic.appendChild(musicName)
-musicName.innerText= `${music.trackName}`
+        // *** name of song *** 
+    const musicName = document.createElement('p')
+    musicName.setAttribute("id", "music-name")
+    eachMusic.appendChild(musicName)
+    musicName.innerText= `${music.trackName}`
 
     // *** photo of each music ***
     const musicImg = document.createElement('img')
@@ -62,7 +69,8 @@ musicName.innerText= `${music.trackName}`
     artSrc.value = `${music.artworkUrl100}`
     //set attribute node to img
     musicImg.setAttributeNode(artSrc)
-    // *** link of each music to play
+
+    // *** link of each music to play **** 
     const musicPlayLink = document.createElement('audio')
     eachMusic.appendChild(musicPlayLink)
     previewSrc = document.createAttribute("src")
@@ -71,4 +79,22 @@ musicName.innerText= `${music.trackName}`
     controlsAttr = document.createAttribute("controls")
     musicPlayLink.setAttributeNode(controlsAttr)
 
+    // *** play-button on the img 
+    const playButton = document.createElement('button')
+    playButton.innerText = "Play"
+    eachMusic.appendChild(playButton)
+    playButton.setAttribute("class", "btn")
+    playButton.setAttribute("id", `${index}`)
+
 }
+
+// click on picture play to each music 
+musicListing.addEventListener('click', event=> {
+    console.log(event.target.id)
+
+})
+
+
+
+
+
