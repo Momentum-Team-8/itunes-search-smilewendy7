9773920f782d5aa9253e2c1ad2880b8d83c99bff
smilewendy7 <smilewendy7@gmail.com>
2021-06-03 20:28:17 -0400

    fix the audio player add basic css

--- main.js
+++ main.js
@@ -2,8 +2,12 @@
     // search + ???
     //favicon where is it?
     //add lines to seperate sections ... 
+    // get rid of the list dot 
 
-const url = "https://proxy-itunes-api.glitch.me/search?"
+// const url = "https://proxy-itunes-api.glitch.me/search?"
+const url = "https://itunes.apple.com/search?"
+
+
 const inputContent = document.querySelector('#search-field').value
 
 const form = document.querySelector('.search-form')
@@ -24,12 +28,12 @@ form.addEventListener('reset', event=> {
 //get the list of searching results 
 function searchSongs(){
     const inputContent = document.querySelector('#search-field').value
-    fetch (url + "entity=song&" + "term="+ `${inputContent}`+ "&limit=10")
+    fetch (url + "entity=song&" + "term="+ `${inputContent}`+ "&limit=36")
     .then(res => res.json())
     .then(data=> {
         // console.log(data)
         console.log(data)
-        console.log("this is the url when searching", url + "entity=song&" + "term="+ `${inputContent}`+ "&limit=10")
+        console.log("this is the url when searching", url + "entity=song&" + "term="+ `${inputContent}`+ "&limit=36")
 
         let i = 0
         for (let result of data.results){
@@ -78,21 +82,37 @@ function renderMusic(music, index) {
     musicPlayLink.setAttributeNode(previewSrc)
     controlsAttr = document.createAttribute("controls")
     musicPlayLink.setAttributeNode(controlsAttr)
+    musicPlayLink.setAttribute("class", "each-audio")
 
     // *** play-button on the img 
     const playButton = document.createElement('button')
     playButton.innerText = "Play"
     eachMusic.appendChild(playButton)
     playButton.setAttribute("class", "btn")
-    playButton.setAttribute("id", `${index}`)
+    playButton.setAttribute("id", `${music.trackId}`)
 
+    // !!!! ------ ********* click on picture play to each music !!!! ------ *********
+        playButton.addEventListener('click', event=> {
+         console.log(event.target.id, music.trackId)
+    // the preview link is in the data base 
+        // if(event.target.id === music.trackId){
+            console.log(music.previewUrl)
+        //}
+            let src = document.createAttribute("src")
+            document.getElementById("music-player").setAttributeNode(src)
+            src.value= music.previewUrl
+
+            // add music name below the player
+            let musicTittle = document.getElementById("music -title")
+            musicTittle.innerText= "Now Playing:"+ music.artistName+ "----"+ music.trackName
+
+        
+        
+    
+    })
 }
 
-// click on picture play to each music 
-musicListing.addEventListener('click', event=> {
-    console.log(event.target.id)
 
-})
 
 
 
